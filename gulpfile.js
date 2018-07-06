d57460b25678db81bbdb2e909e72891dab6a79c4
Ivan Didenko <32499861+ivandidenkoweb@users.noreply.github.com>
2018-07-06 16:15:08 +0300

    Поставил gulp + less

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var path = require("path");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var browserSync = require("browser-sync").create();
+
+
+gulp.task("style", function () {
+  return gulp.src("less/style.less")
+  	.pipe(plumber())
+    .pipe(less({
+      paths: [ path.join(__dirname, "less", "includes") ]
+    }))
+    .pipe(postcss([
+    	autoprefixer()
+    	]))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+	browserSync.init({
+		server: ".",
+		notify: false,
+		open: true,
+		cors: true,
+		ui: false
+	});
+
+	gulp.watch("less/**/*.less", ["style"]);
+	gulp.watch("*.html").on("change", browserSync.reload);
+}); 
